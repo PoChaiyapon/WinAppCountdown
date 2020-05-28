20b5a0be2bcff6bc50941d63d9fd5f66686fd3bb
Chaiyapon <chaiporn137@gmail.com>
2020-05-28 09:05:49 +0700

    Upload file to remote repo

---
+++ CountDownTimer/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace CountDownTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblTimeNow = new System.Windows.Forms.Label();
+            this.lblMinute = new System.Windows.Forms.Label();
+            this.lblDown = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.lblDown);
+            this.groupBox1.Controls.Add(this.lblMinute);
+            this.groupBox1.Controls.Add(this.lblTimeNow);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(14, 48);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(367, 142);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "จับเวลา";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(59, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "เวลาขณะนี้ :";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "นับเวลาถอยหลัง :";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(65, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "เหลือเวลา :";
+            // 
+            // lblTimeNow
+            // 
+            this.lblTimeNow.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTimeNow.AutoSize = true;
+            this.lblTimeNow.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimeNow.Location = new System.Drawing.Point(147, 32);
+            this.lblTimeNow.Name = "lblTimeNow";
+            this.lblTimeNow.Size = new System.Drawing.Size(18, 20);
+            this.lblTimeNow.TabIndex = 0;
+            this.lblTimeNow.Text = "- ";
+            // 
+            // lblMinute
+            // 
+            this.lblMinute.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblMinute.AutoSize = true;
+            this.lblMinute.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMinute.Location = new System.Drawing.Point(147, 63);
+            this.lblMinute.Name = "lblMinute";
+            this.lblMinute.Size = new System.Drawing.Size(18, 20);
+            this.lblMinute.TabIndex = 0;
+            this.lblMinute.Text = "- ";
+            // 
+            // lblDown
+            // 
+            this.lblDown.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblDown.AutoSize = true;
+            this.lblDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDown.ForeColor = System.Drawing.Color.Red;
+            this.lblDown.Location = new System.Drawing.Point(147, 98);
+            this.lblDown.Name = "lblDown";
+            this.lblDown.Size = new System.Drawing.Size(18, 20);
+            this.lblDown.TabIndex = 0;
+            this.lblDown.Text = "- ";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(47, 11);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(58, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(111, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 16);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "วินาที";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(165, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 202);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Name = "Form1";
+            this.Text = "โปรแกรมการนับเวลาถอยหลัง";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblDown;
+        private System.Windows.Forms.Label lblMinute;
+        private System.Windows.Forms.Label lblTimeNow;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CountDownTimer/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CountDownTimer
+{
+    public partial class Form1 : Form
+    {
+        long countMin;
+        int second1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblTimeNow.Text = DateTime.Now.ToLongTimeString().ToString();
+            lblMinute.Text = textBox1.Text.Trim()+ " วินาที";
+            lblDown.Text = textBox1.Text.Trim();
+            ////Timer จะถูกกระตุ้น (Trigger) การทำงานทุกๆ 1 วินาที (1000 millisecond)
+            //timer1.Interval = 1000;
+            //timer1.Enabled = true;
+            ////ตั้งนับถอยหลัง 1 นาที (60 x 1000 ได้หน่วยนับมิลลิวินาที)
+            ////countMin = 60000;
+            //countMin = Int32.Parse(textBox1.Text.Trim()) * 1000;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblTimeNow.Text = DateTime.Now.ToLongTimeString().ToString();
+            countMin = countMin - 1000;
+            //============================ แสดงผลเวลาการนับถอยหลัง =========================
+            //lblDown.Text = (Int32.Parse(lblDown.Text) - 1).ToString();
+            //if (countMin == 0)
+            //{
+            //    lblDown.Text = "หมดเวลา/Logout Time";
+            //    //ปิดการทำงานของ Timer
+            //    //timer1.Enabled = false;
+            //}
+
+            if(countMin > 0)
+            {
+                lblDown.Text = (Int32.Parse(lblDown.Text) - 1).ToString();
+            }
+            else
+            {
+                lblDown.Text = "หมดเวลา/Logout Time";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(string.IsNullOrEmpty(textBox1.Text.Trim()))
+                {
+                    MessageBox.Show("กรุณากำหนดจำนวน วินาที !","error");
+                    return;
+                }
+                else
+                {
+                    lblMinute.Text = textBox1.Text.Trim() + " วินาที";
+                    lblDown.Text = textBox1.Text.Trim();
+                    countMin = Int32.Parse(textBox1.Text.Trim()) * 1000;
+                    //Timer จะถูกกระตุ้น (Trigger) การทำงานทุกๆ 1 วินาที (1000 millisecond)
+                    timer1.Interval = 1000;
+                    timer1.Enabled = true;
+                    //ตั้งนับถอยหลัง 1 นาที (60 x 1000 ได้หน่วยนับมิลลิวินาที)
+                    second1 = string.IsNullOrEmpty(textBox1.Text.Trim()) ? 0 : Int32.Parse(textBox1.Text.Trim());
+                    countMin = second1 * 1000;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
